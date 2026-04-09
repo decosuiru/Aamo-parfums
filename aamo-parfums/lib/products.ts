9683dbe161b473209ff42553d0a8e1adcc2fb927
Tom <89904434+decosuiru@users.noreply.github.com>
2026-04-09 09:58:00 +0000

    commit

--- aamo-parfums/lib/products.ts
+++ aamo-parfums/lib/products.ts
@@ -28,7 +28,7 @@ export const products: Product[] = [
       heart: "Juniper Bay, Ginger, Nutmeg, Geranium",
       base: "Patchouli, Vetiver, Cedarwood, Moss"
     },
-    images: ["/images/PNG/emit-light.png", "/images/PNG/emit-light.png"]
+    images: ["/images/PNG/emit-light.png", "/images/emit-light-2.jpg"]
   },
   {
     id: "glace-amor",
